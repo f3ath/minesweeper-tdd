2132ddd56523882b03beec229924ccc5491348db
Alexey Karapetov <karapetov@gmail.com>
2017-02-25 13:40:15 -0800

    Extracted Board

---
+++ src/Board.php
@@ -0,0 +1,8 @@
+<?php
+namespace F3\Minesweeper;
+
+
+class Board
+{
+
+}
---
+++ src/Rectangle.php
@@ -0,0 +1,9 @@
+<?php
+namespace F3\Minesweeper;
+
+interface Rectangle
+{
+    public function getHeight(): int;
+
+    public function getWidth(): int;
+}
